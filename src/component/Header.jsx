1cc4639468729a1bd78972fd44ff3676fb26266a
wondmeneh123 <wondmenhfekadu@gmail.com>
2024-07-03 12:27:38 +0000

    first

---
+++ src/component/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+const Header = () => {
+    return (
+        <>gg</>
+    );
+}
+
+export default Header
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../component/Header'
+import SIdebar from '../component/SIdebar'
+const Home = () => {
+  return (
+    <div>
+        <Header />
+      
+        <SIdebar className='mt-60'/>
+    </div>
+  )
+}
+
+export default Home
